2050d97da1666a9cb8515ed0bccc4c7380f4fc97
arty gvozdenkov <gvozdenkov@gmail.com>
2024-03-13 09:39:42 +0300

    docs: add openapi.yaml spec & swagger serve

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import YAML from 'js-yaml';
+import fs from 'fs';
 import { config } from '#config';
 import { AppModule } from './app.module';
 
-var { port: PORT } = config;
+var { port: PORT, basePath: BASE_PATH } = config;
 
 var bootstrap = () => {
-  NestFactory.create(AppModule).then((app) =>
+  NestFactory.create(AppModule).then((app) => {
+    var openApiSpec = YAML.load(fs.readFileSync('docs/openapi.yaml', 'utf-8'));
+
+    SwaggerModule.setup(`${BASE_PATH}/v1/docs`, app, openApiSpec);
+
     // eslint-disable-next-line no-console
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}, http://localhost:${PORT}`)),
-  );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}, http://localhost:${PORT}`));
+  });
 };
 
 bootstrap();
