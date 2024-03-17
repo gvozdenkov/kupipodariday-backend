bfe7b1ceb42bcfd17a6fb11a1ea19753ccf83e19
arty gvozdenkov <gvozdenkov@gmail.com>
2024-03-17 17:03:42 +0300

    feat: connect to db and link User resource in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User, UsersService, UsersController } from '#users';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'admin',
+      password: 'root',
+      database: 'postgres',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+  ],
 })
 export class AppModule {}
