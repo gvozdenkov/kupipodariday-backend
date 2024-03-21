6e139181501bc3e57aeb82aee1d9ba6d0789e603
arty gvozdenkov <gvozdenkov@gmail.com>
2024-03-21 11:23:03 +0300

    refactor: validate & defaul values in user.enity only

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,24 +1,10 @@
-import { IsString, IsEmail, IsUrl, Length, IsOptional } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { User } from '#users/entities/user.entity';
+import { PickType } from '@nestjs/swagger';
 
-export class CreateUserDto {
-  @IsString()
-  @Length(2, 64, { message: `'username' should have minium 2 and maximum 64 characters` })
-  username: string;
-
-  @IsOptional()
-  @IsString()
-  @Length(0, 200, { message: `'about' should have maximum 200 characters` })
-  about: string = '';
-
-  @IsOptional()
-  @IsUrl({}, { message: `'avatar' should be a valid link to the image` })
-  avatar: string = `https://i.pravatar.cc/200/?img=${Math.floor(Math.random() * 70) + 1}`;
-
-  @IsEmail()
-  @Transform((param) => param.value.toLowerCase())
-  email: string;
-
-  @Length(4, 64, { message: `'password' shoud be minimum 4 and maximum 64 charecters` })
-  password: string;
-}
+export class CreateUserDto extends PickType(User, [
+  'username',
+  'about',
+  'avatar',
+  'email',
+  'password',
+]) {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,5 @@
+import { Transform } from 'class-transformer';
+import { IsEmail, IsOptional, IsString, IsUrl, Length } from 'class-validator';
 import {
   Entity,
   PrimaryGeneratedColumn,
@@ -12,18 +14,28 @@ export class User {
   id: string;
 
   @Column()
+  @IsString()
+  @Length(2, 64, { message: `'username' should have minium 2 and maximum 64 characters` })
   username: string;
 
   @Column()
+  @IsOptional()
+  @IsString()
+  @Length(0, 200, { message: `'about' should have maximum 200 characters` })
   about: string;
 
   @Column()
-  avatar: string;
+  @IsOptional()
+  @IsUrl({}, { message: `'avatar' should be a valid link to the image` })
+  avatar: string = `https://i.pravatar.cc/200/?img=${Math.floor(Math.random() * 70) + 1}`;
 
   @Column({ unique: true })
+  @IsEmail()
+  @Transform((param) => param.value.toLowerCase())
   email: string;
 
   @Column({ select: false })
+  @Length(4, 64, { message: `'password' shoud be minimum 4 and maximum 64 charecters` })
   password: string;
 
   @CreateDateColumn({ select: false })
